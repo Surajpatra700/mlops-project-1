568d17b7ca9e7c1cd42c3a87ee42d58caadefd2c
Suraj Patra <surajpatra2908@gmail.com>
2024-05-06 23:59:52 +0530

    dgd

--- model.py
+++ model.py
@@ -1,38 +1,38 @@
-# Import libraries
-from tensorflow.keras.datasets import mnist # type: ignore
-from tensorflow.keras.models import Sequential # type: ignore
-from tensorflow.keras.layers import Dense # type: ignore
-from tensorflow.keras.utils import to_categorical # type: ignore
+# # Import libraries
+# from tensorflow.keras.datasets import mnist # type: ignore
+# from tensorflow.keras.models import Sequential # type: ignore
+# from tensorflow.keras.layers import Dense # type: ignore
+# from tensorflow.keras.utils import to_categorical # type: ignore
 
-# Load the MNIST dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# # Load the MNIST dataset
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Preprocess data
-X_train = X_train.reshape((X_train.shape[0], 28 * 28)).astype('float32') / 255
-X_test = X_test.reshape((X_test.shape[0], 28 * 28)).astype('float32') / 255
+# # Preprocess data
+# X_train = X_train.reshape((X_train.shape[0], 28 * 28)).astype('float32') / 255
+# X_test = X_test.reshape((X_test.shape[0], 28 * 28)).astype('float32') / 255
 
-# One-hot encode labels
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+# # One-hot encode labels
+# y_train = to_categorical(y_train)
+# y_test = to_categorical(y_test)
 
-# Define the model
-model = Sequential()
-model.add(Dense(32, activation='relu', input_dim=28 * 28))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+# # Define the model
+# model = Sequential()
+# model.add(Dense(32, activation='relu', input_dim=28 * 28))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dense(10, activation='softmax'))
 
-# Compile the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# # Compile the model
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Train the model
-model.fit(X_train, y_train, epochs=5, batch_size=128)
+# # Train the model
+# model.fit(X_train, y_train, epochs=5, batch_size=128)
 
-# Evaluate the model
-loss, accuracy = model.evaluate(X_test, y_test)
-print('Test accuracy:', accuracy)
+# # Evaluate the model
+# loss, accuracy = model.evaluate(X_test, y_test)
+# print('Test accuracy:', accuracy)
 
-# Predict on a new image (optional)
-# new_image = ... (prepare your new image data here)
-# prediction = model.predict(new_image)
-# predicted_class = np.argmax(prediction) 
-# print('Predicted class:', predicted_class)
+# # Predict on a new image (optional)
+# # new_image = ... (prepare your new image data here)
+# # prediction = model.predict(new_image)
+# # predicted_class = np.argmax(prediction) 
+# # print('Predicted class:', predicted_class)
